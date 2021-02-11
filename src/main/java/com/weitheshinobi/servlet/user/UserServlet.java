46b87d39c059c6ba6d55324e06ca074d1c15322a
KaoWei <weitheshinobi@gmail.com>
2021-02-11 17:27:49 +0800

    密碼修改系統 *ajax

--- src/main/java/com/weitheshinobi/servlet/user/UserServlet.java
+++ src/main/java/com/weitheshinobi/servlet/user/UserServlet.java
@@ -1,6 +1,8 @@
 package com.weitheshinobi.servlet.user;
 
+import com.alibaba.fastjson.JSONArray;
 import com.mysql.cj.util.StringUtils;
+import com.mysql.cj.xdevapi.JsonArray;
 import com.weitheshinobi.pojo.User;
 import com.weitheshinobi.service.user.UserService;
 import com.weitheshinobi.service.user.UserServiceImpl;
@@ -11,10 +13,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method = req.getParameter("method");
+
+        if(method.equals("savepwd") && method!=null){
+            this.updatePwd(req,resp);
+        }else if(method.equals("pwdmodify") &&method!=null){
+            this.pwdModify(req,resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    public void updatePwd(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Object o = req.getSession().getAttribute(Constants.USER_SESSION);
         String newPassword = req.getParameter("newpassword");
 
@@ -35,8 +55,34 @@ public class UserServlet extends HttpServlet {
         req.getRequestDispatcher("pwdmodify.jsp").forward(req,resp);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+    private void pwdModify(HttpServletRequest req, HttpServletResponse resp) {
+        Object o = req.getSession().getAttribute(Constants.USER_SESSION);
+        String oldpassword = req.getParameter("oldpassword");
+        Map<String, String> resultMap = new HashMap<String, String>();
+
+        if(o == null){//session过期
+            resultMap.put("result", "sessionerror");
+        }else if(StringUtils.isNullOrEmpty(oldpassword)){//旧密码输入为空
+            resultMap.put("result", "error");
+        }else{
+            String sessionPwd = ((User)o).getUserPassword();
+            if(oldpassword.equals(sessionPwd)){
+                resultMap.put("result", "true");
+            }else{//旧密码输入不正确
+                resultMap.put("result", "false");
+            }
+        }
+
+        try {
+            resp.setContentType("application/json");
+            PrintWriter writer = resp.getWriter();
+            writer.write(JSONArray.toJSONString(resultMap));
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
